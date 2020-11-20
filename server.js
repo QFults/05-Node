e2f9682e158e9f6b7d30f160472566e811548842
Quinton Fults <quinton.fults.work@gmail.com>
2020-11-20 13:19:16 -0800

    code

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const http = require('http')
 
-const server = http.createServer()
+const server = http.createServer((req, res) => {
+  console.log(req.url)
 
-server.listen(3000)
+  if (req.url === '/greeting') {
+    res.end('<h1>Hello World!</h1>')
+  } else {
+    res.end('404 Not Found')
+  }
+
+})
+
+server.listen(3000, () => console.log('http://localhost:3000'))
